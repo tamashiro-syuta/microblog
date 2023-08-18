d2379bea2e1abc483bb304c3040895a7f49f367c
tamashiro-syuta <syuta5825@gmail.com>
2023-08-18 14:52:30 +0900

    ✨ 詳細ページを追加

--- microblog/urls.py
+++ microblog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import frontpage
+from blog.views import frontpage, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', frontpage),
+    path('<slug:slug>/', post_detail, name='post_detail'),
 ]
